938953094d0bbd7452e2f2e1c9878330ca21d148
Tomas Panik <tomas.panik@covered.lht>
2016-08-28 23:41:39 +0200

    adjusted deployment

--- src/CryptMaster/Program.cs
+++ src/CryptMaster/Program.cs
@@ -3,12 +3,23 @@ namespace CryptMaster
     using System.IO;
     using Microsoft.AspNetCore.Builder;
     using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Configuration;
 
     public class Program
     {
         public static void Main(string[] args)
         {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("hosting.json", optional: true)
+                .AddEnvironmentVariables(prefix: "ASPNETCORE_")
+                .AddCommandLine(args)
+                .Build();
+
             var host = new WebHostBuilder()
+                .UseUrls("http://0.0.0.0:5000")
+                .UseEnvironment("Development")
+                .UseConfiguration(config)
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
